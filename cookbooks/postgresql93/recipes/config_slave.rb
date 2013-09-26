062f976eaa967a2732a18648d10f7873c49530d6
Satoshi Nagayasu <snaga@uptime.jp>
2013-09-26 11:23:24 +0900

    Add new recipes for PostgreSQL master-slave replication configuration.

---
+++ cookbooks/postgresql93/recipes/config_slave.rb
@@ -0,0 +1,38 @@
+#
+# Cookbook Name:: postgresql93
+# Recipe:: initdb
+#
+# Copyright 2013, Uptime Technologies, LLC.
+#
+# All rights reserved - Do Not Redistribute
+#
+bash "postgresql93-pgbasebackup" do
+  not_if { File.exists?("/var/lib/pgsql/9.3/data/PG_VERSION") }
+  code <<-EOC
+    sudo -u postgres /usr/pgsql-9.3/bin/pg_basebackup -D /var/lib/pgsql/9.3/data -h 192.168.1.102 -U replication --xlog --checkpoint=fast --progress
+  EOC
+end
+
+template "postgresql-9.3" do
+  path "/etc/sysconfig/pgsql/postgresql-9.3"
+  source "postgresql-9.3.erb"
+  owner "root"
+  group "root"
+  mode 0644
+#  notifies :restart, 'service[postgresql-9.3]'
+end
+
+template "recovery.conf" do
+  path "/var/lib/pgsql/9.3/data/recovery.conf"
+  source "recovery.conf.erb"
+  owner "postgres"
+  group "postgres"
+  mode 0600
+#  notifies :reload, 'service[postgresql-9.3]'
+end
+
+service "postgresql-9.3" do
+  supports :status => true , :restart => true , :reload => true
+  action [ :enable, :start ]
+end
+
--- solo-vagrant.rb
+++ solo-vagrant.rb
@@ -1,2 +1,3 @@
 file_cache_path "/tmp/chef-solo"
 cookbook_path ["/vagrant/chef-pgsql/cookbooks"]
+role_path "/vagrant/chef-pgsql/roles"
